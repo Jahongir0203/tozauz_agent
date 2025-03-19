06bdf8dc77fe573c94f9f3a948d9d30f9dd04b71
jahongireshonqulov <jahongireshonqulov17@gmail.com>
2025-03-20 01:00:42 +0500

    feat:some changes

---
+++ lib/core/utils/locale_keys.g.dart
@@ -0,0 +1,45 @@
+// DO NOT EDIT. This is code generated via package:easy_localization/generate.dart
+
+// ignore_for_file: constant_identifier_names
+
+abstract class  LocaleKeys {
+  static const reports = 'reports';
+  static const wallet = 'wallet';
+  static const profile = 'profile';
+  static const selectLang = 'selectLang';
+  static const containers = 'containers';
+  static const reDownload = 'reDownload';
+  static const earnings = 'earnings';
+  static const lastEmpty = 'lastEmpty';
+  static const price = 'price';
+  static const status = 'status';
+  static const active = 'active';
+  static const notActive = 'notActive';
+  static const simModule = 'simModule';
+  static const address = 'address';
+  static const phoneNumber = 'phoneNumber';
+  static const balance = 'balance';
+  static const updatePassword = 'updatePassword';
+  static const logOut = 'logOut';
+  static const changeLanguage = 'changeLanguage';
+  static const newReports = 'newReports';
+  static const reportsHistory = 'reportsHistory';
+  static const numOfContainers = 'numOfContainers';
+  static const typeOfContainers = 'typeOfContainers';
+  static const oneContainer = 'oneContainer';
+  static const totalPayment = 'totalPayment';
+  static const payWithBalance = 'payWithBalance';
+  static const withCard = 'withCard';
+  static const sendReport = 'sendReport';
+  static const newPaymentRequest = 'newPaymentRequest';
+  static const paymentRequestHistory = 'paymentRequestHistory';
+  static const paymentRequest = 'paymentRequest';
+  static const amount = 'amount';
+  static const enterAmount = 'enterAmount';
+  static const cardNumber = 'cardNumber';
+  static const enterYourCardNumber = 'enterYourCardNumber';
+  static const sendPaymentRequest = 'sendPaymentRequest';
+  static const expecting = 'expecting';
+  static const date = 'date';
+
+}
